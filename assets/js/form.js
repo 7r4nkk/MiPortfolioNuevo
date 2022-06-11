fb4b8a011b493a7f500e792d09c9fc86d0bee197
frankfobello <fobellofrank@gmail.com>
2022-06-11 17:28:32 -0300

    Form configurado

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,21 +1,4 @@
-const $form = document.querySelector('#form');
-$form.addEventListener('submit', handleSubmit);
 
-async function handleSubmit(e) {
-    e.preventDefault();
-    const form = new FormData(this)
-    const response = await fetch('this.action', {
-        method: this.netlify,
-        body: form,
-        headers: {
-            'Accept': 'application/json',
-        }
-    })
-    if (response.ok) {
-        this.reset
-        alert('Thank you for your message!')
-    }
-}
 
 
 
