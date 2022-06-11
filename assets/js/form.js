c7f66e14e8a42965619c414f3a034ffde02018f6
frankfobello <fobellofrank@gmail.com>
2022-06-11 17:03:31 -0300

    Form agregado

---
+++ assets/js/form.js
@@ -0,0 +1,14 @@
+/* const $form = document.querySelector('#form');
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const form = new FormData(this)
+    console.log(form);
+} */
+
+
+
+
+
+
