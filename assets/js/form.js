23b5e6fd23740c61a272ac89a93dd4b78f25fc9d
frankfobello <fobellofrank@gmail.com>
2022-06-11 17:15:10 -0300

    Prueba de formulario

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,11 +1,21 @@
-/* const $form = document.querySelector('#form');
+const $form = document.querySelector('#form');
 $form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
     e.preventDefault();
     const form = new FormData(this)
-    console.log(form);
-} */
+    const response = await fetch('this.action', {
+        method: this.netlify,
+        body: form,
+        headers: {
+            'Accept': 'application/json',
+        }
+    })
+    if (response.ok) {
+        this.reset
+        alert('Thank you for your message!')
+    }
+}
 
 
 
